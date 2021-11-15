930f1bfd69d6ab824b6e65f9ad837e169c8bea41
Yaroslav Moiseev <deleted91115@gmail.com>
2021-11-15 08:56:22 +0300

    дохера чего изменилось. поправил ошибки рендера; нащупал дальнейшие варианты разработки (HttpClient от апач может помочь) все изменения в основном в MainActivity

--- app/src/main/java/com/example/probapera/MainActivity.java
+++ app/src/main/java/com/example/probapera/MainActivity.java
@@ -5,15 +5,32 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-//import org.jsoup.Jsoup;
-
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import cz.msebera.android.httpclient.HttpEntity;
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+
 
 public class MainActivity extends AppCompatActivity {
     private EditText login;
     private EditText password;
     private Button signIn;
+    private Thread secondThread;
+    private Runnable runnable;
+    private HttpClient client = new DefaultHttpClient();
+    private HttpGet request;
+    private HttpResponse response;
+    private HttpPost post;
+    private HttpEntity siteEntity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +40,42 @@ public class MainActivity extends AppCompatActivity {
         login = findViewById(R.id.Login);
         password = findViewById(R.id.Password);
         signIn = findViewById(R.id.signInButton);
+        try {
+            BufferedReader rd = new BufferedReader
+                    (new InputStreamReader(
+                            response.getEntity().getContent()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(login.getText().toString().trim().equals("") && password.getText().toString().trim().equals(""))
-                Toast.makeText(MainActivity.this,R.string.nothingInText, Toast.LENGTH_LONG).show();
 
-
-            }
-        });
     }
+    public void init() {
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                request = new HttpGet("http://portal.mfmgutu.ru/index_ins.php?bl=1&idpg=915");
+                try {
+                    response = client.execute(request);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                siteEntity = response.getEntity();
+                System.out.println(siteEntity);
+            }
+        };
+        secondThread = new Thread(runnable);
+        secondThread.start();
 
-
-
+    }
+    public void Click(View view) {
+        if (login.getText().toString().trim().equals("") && password.getText().toString().trim().equals("")) {
+            Toast.makeText(MainActivity.this, R.string.nothingInText, Toast.LENGTH_LONG).show();
+        } else {
+            init();
+            /*Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);*/
+        }
+    }
 }
+
